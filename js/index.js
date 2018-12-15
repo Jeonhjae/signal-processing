e4c8b7691d7f7dc882b62efe570ee039eda9ae7d
Jeonhjae <38119733+Jeonhjae@users.noreply.github.com>
2018-12-15 18:18:06 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,27 @@
 window.onload = function () {
- alert("로딩 완료");
+ // based on prepared DOM, initialize echarts instance
+        var myChart = echarts.init(document.getElementById('test'));
+
+        // specify chart configuration item and data
+        var option = {
+            title: {
+                text: 'ECharts entry example'
+            },
+            tooltip: {},
+            legend: {
+                data:['Sales']
+            },
+            xAxis: {
+                data: ["shirt","cardign","chiffon shirt","pants","heels","socks"]
+            },
+            yAxis: {},
+            series: [{
+                name: 'Sales',
+                type: 'bar',
+                data: [5, 20, 36, 10, 10, 20]
+            }]
+        };
+
+        // use configuration item and data specified to show chart
+        myChart.setOption(option);
 }
