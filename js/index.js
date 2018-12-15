76a3b632d5ca5faab893e871bccf6a63a6dc73e9
Jeonhjae <38119733+Jeonhjae@users.noreply.github.com>
2018-12-15 19:15:53 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,107 +2,40 @@ window.onload = function () {
  // based on prepared DOM, initialize echarts instance
  var myChart = echarts.init(document.getElementById('test'));
  myChart.showLoading();
- let data = [{
-  category: 0
-  draggable: false
-  id: "1"
-  itemStyle: null
-  name: "12"
-  symbolSize: 10
-  value: 10
-  x: null
-  y: null
- }, {
-  category: 0
-  draggable: false
-  id: "1"
-  itemStyle: null
-  name: "13"
-  symbolSize: 10
-  value: 10
-  x: null
-  y: null
- }, {
-  category: 0
-  draggable: false
-  id: "1"
-  itemStyle: null
-  name: "14"
-  symbolSize: 10
-  value: 10
-  x: null
-  y: null
- }, {
-  category: 0
-  draggable: false
-  id: "1"
-  itemStyle: null
-  name: "15"
-  symbolSize: 10
-  value: 10
-  x: null
-  y: null
- }, {
-  category: 1
-  draggable: false
-  id: "1"
-  itemStyle: null
-  name: "26"
-  symbolSize: 10
-  value: 10
-  x: null
-  y: null
- },{
-  category: 1
-  draggable: false
-  id: "1"
-  itemStyle: null
-  name: "64"
-  symbolSize: 10
-  value: 10
-  x: null
-  y: null
- }
-]
+ $.get('https://ecomfe.github.io/echarts-examples/public/data/asset/data/webkit-dep.json', function (webkitDep) {
     myChart.hideLoading();
 
-   
     option = {
-        title: {
-            text: 'Les Miserables',
-            subtext: 'Default layout',
-            top: 'bottom',
-            left: 'right'
+        legend: {
+            data: ['HTMLElement', 'WebGL', 'SVG', 'CSS', 'Other']
         },
-        tooltip: {},
-        legend: [{
-            // selectedMode: 'single',
-            data: categories.map(function (a) {
-                return a.name;
-            })
-        }],
-        animation: false,
-        series : [
-            {
-                name: 'Les Miserables',
-                type: 'graph',
-                layout: 'force',
-                data: graph.nodes,
-                links: graph.links,
-                categories: categories,
-                roam: true,
-                label: {
-                    normal: {
-                        position: 'right'
-                    }
-                },
-                force: {
-                    repulsion: 100
+        series: [{
+            type: 'graph',
+            layout: 'force',
+            animation: false,
+            label: {
+                normal: {
+                    position: 'right',
+                    formatter: '{b}'
                 }
-            }
-        ]
+            },
+            draggable: true,
+            data: webkitDep.nodes.map(function (node, idx) {
+                node.id = idx;
+                return node;
+            }),
+            categories: webkitDep.categories,
+            force: {
+                // initLayout: 'circular'
+                // repulsion: 20,
+                edgeLength: 5,
+                repulsion: 20,
+                gravity: 0.2
+            },
+            edges: webkitDep.links
+        }]
     };
 
     myChart.setOption(option);
-}, 'xml');
+});
 }
