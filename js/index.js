a0cd53d39ca34a02d53b2117382ff38842b784cc
Jeonhjae <38119733+Jeonhjae@users.noreply.github.com>
2018-12-15 18:20:04 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,27 +1,29 @@
 window.onload = function () {
  // based on prepared DOM, initialize echarts instance
-        var myChart = echarts.init(document.getElementById('test'));
+ var myChart = echarts.init(document.getElementById('test'));
+ myChart.showLoading();
+ /*
+ // specify chart configuration item and data
+ var option = {
+     title: {
+         text: 'ECharts entry example'
+     },
+     tooltip: {},
+     legend: {
+         data:['Sales']
+     },
+     xAxis: {
+         data: ["shirt","cardign","chiffon shirt","pants","heels","socks"]
+     },
+     yAxis: {},
+     series: [{
+         name: 'Sales',
+         type: 'bar',
+         data: [5, 20, 36, 10, 10, 20]
+     }]
+ };
 
-        // specify chart configuration item and data
-        var option = {
-            title: {
-                text: 'ECharts entry example'
-            },
-            tooltip: {},
-            legend: {
-                data:['Sales']
-            },
-            xAxis: {
-                data: ["shirt","cardign","chiffon shirt","pants","heels","socks"]
-            },
-            yAxis: {},
-            series: [{
-                name: 'Sales',
-                type: 'bar',
-                data: [5, 20, 36, 10, 10, 20]
-            }]
-        };
-
-        // use configuration item and data specified to show chart
-        myChart.setOption(option);
+ // use configuration item and data specified to show chart
+ myChart.setOption(option);
+ */
 }
